38b15a9b149ff9e7ab3a9b32f06186d601744d9b
Tara Martin <martin.tara117@gmail.com>
2020-11-02 11:44:22 -0500

    added questions to the question var

--- script.js
+++ script.js
@@ -2,13 +2,41 @@ var inquirer = require("inquirer");
 
 var questions = [
   {
-    name: "Title",
+    name: "title",
     message: "What is the title of your project?",
+  },
+  {
+    name: "description",
+    message: "Please describe your project.",
+  },
+  {
+    name: "installation",
+    message: "What are you installation instructions?",
+  },
+  {
+    name: "usage",
+    message: "Please provide usage information.",
+  },
+  {
+    name: "contribution",
+    message: "What are you contribution guidelines?",
+  },
+  {
+    name: "test",
+    message: "How do you test your project?",
+  },
+  {
+    name: "gitHub",
+    message: "Please provide a link to your Git Hub account.",
+  },
+  {
+    name: "email",
+    message: "Please provide an e-mail to add to your contact information.",
   },
 ];
 
 inquirer.prompt(questions).then(gotAnswers);
 
-function gotAnswers(answers){
-    console.log("your title is", answers.Title);
+function gotAnswers(answers) {
+  console.log(answers);
 }
