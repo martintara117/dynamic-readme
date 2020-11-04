d53f698f5ebb1187f44b58a2c06ceb0480454e14
Tara Martin <martin.tara117@gmail.com>
2020-11-04 16:12:03 -0500

    created licensing fuction with the description and badge. placed license badge at the top of the readme and added licensing description to the License portion of the readme.

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,6 +1,29 @@
 // function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  const licensing = {
+    "Apache 2.0": {
+      description:
+        "A permissive license whose main conditions require preservation of copyright and license notices. Contributors provide an express grant of patent rights. Licensed works, modifications, and larger works may be distributed under different terms and without source code.",
+      badge:
+        "[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)",
+    },
+    "GPL 3.0": {
+      description:
+        "Permissions of this strong copyleft license are conditioned on making available complete source code of licensed works and modifications, which include larger works using a licensed work, under the same license. Copyright and license notices must be preserved. Contributors provide an express grant of patent rights.",
+      badge:
+        "[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)",
+    },
+    MIT: {
+      description:
+        "A short and simple permissive license with conditions only requiring preservation of copyright and license notices. Licensed works, modifications, and larger works may be distributed under different terms and without source code.",
+      badge:
+        "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)",
+    },
+  };
+
+  return `
+  ${licensing[data.license].badge}
+  # ${data.title}
 ## Description 
 ${data.description}
 
@@ -9,7 +32,7 @@ ${data.description}
 * [Usage](#Usage)
 * [License](#License)
 * [Contributing](#Contributing)
-* [Tests](#Tests)
+* [Test](#Test)
 * [Contact](#Contact)
 
 ## Installation
@@ -19,7 +42,7 @@ ${data.installation}
 ${data.usage}
 
 ## License
-${data.license}
+${data.license}: ${licensing[data.license].description}
 
 ## Contributing
 ${data.contribution}
